c4f8b319dd706f1039673e0b79482b5e825511ab
Emerson Reyes <reyesemerson643@gmail.com>
2026-06-18 12:09:27 -0500

    feat: enable CORS for frontend development origins in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  app.enableCors({
+    origin: ['http://localhost:8081', 'http://localhost:8082'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(globalValidationPipe);
   app.useGlobalFilters(new HttpExceptionFilter());
 
